84dadc463fd8138991f81fd3f1ce7509e31620e6
unknown <zlatovvd@ukr.net>
2025-03-20 10:56:19 +0200

    2003

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
 const refs = {
     servicesBtn: document.querySelector(".services-btn"),
-    closeBtn: document.querySelector(".modal__close_btn"),
-    servicesModal: document.querySelector(".backdrop"),
+    closeServicesBtn: document.querySelector("[data-services-close]"),
+    closeHeroBtn: document.querySelector("[data-hero-close]"),
+    heroBtn: document.querySelector(".hero-btn"),
+    servicesModal: document.querySelector("[data-services]"),
+    heroModal: document.querySelector("[data-hero]"),
+    anchors: document.querySelectorAll('a[href*="#"]'),
 
 };
 
 const toggleSevicesModal = () => {
-    console.log("click");
     refs.servicesModal.classList.toggle("is-hidden");
 };
 
+const toggleHeroModal = () => {
+    refs.heroModal.classList.toggle("is-hidden");
+}
+
 refs.servicesBtn.addEventListener("click", toggleSevicesModal);
-refs.closeBtn.addEventListener("click", toggleSevicesModal);
+refs.closeServicesBtn.addEventListener("click", toggleSevicesModal);
+refs.heroBtn.addEventListener("click", toggleHeroModal);
+refs.closeHeroBtn.addEventListener("click", toggleHeroModal);
 
-
-console.log("he");
-console.log(refs.servicesBtn)
+for(let anchor of refs.anchors) {
+    anchor.addEventListener("click", (event) => {
+        event.preventDefault();
+        const blockId = anchor.getAttribute('href');
+        document.querySelector(blockId).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
