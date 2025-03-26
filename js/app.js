349a3777a0d2204833ff4545a8fd1ab2692cecd5
unknown <zlatovvd@ukr.net>
2025-03-26 11:56:25 +0200

    2603

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ const refs = {
     servicesBtn: document.querySelector(".services-btn"),
     closeServicesBtn: document.querySelector("[data-services-close]"),
     closeHeroBtn: document.querySelector("[data-hero-close]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
     heroBtn: document.querySelector(".hero-btn"),
     servicesModal: document.querySelector("[data-services]"),
     heroModal: document.querySelector("[data-hero]"),
+    menuModal: document.querySelector("[data-menu]"),
     anchors: document.querySelectorAll('a[href*="#"]'),
 
 };
@@ -17,15 +19,21 @@ const toggleHeroModal = () => {
     refs.heroModal.classList.toggle("is-hidden");
 }
 
+const toggleMenuModal = () => {
+    refs.menuModal.classList.toggle("is-visible");
+}
+
 refs.servicesBtn.addEventListener("click", toggleSevicesModal);
 refs.closeServicesBtn.addEventListener("click", toggleSevicesModal);
 refs.heroBtn.addEventListener("click", toggleHeroModal);
 refs.closeHeroBtn.addEventListener("click", toggleHeroModal);
+refs.closeMenuBtn.addEventListener("click", toggleMenuModal);
 
 for(let anchor of refs.anchors) {
     anchor.addEventListener("click", (event) => {
         event.preventDefault();
         const blockId = anchor.getAttribute('href');
+        refs.menuModal.classList.remove("is-visible");
         document.querySelector(blockId).scrollIntoView({
             behavior: "smooth",
             block: "start"
